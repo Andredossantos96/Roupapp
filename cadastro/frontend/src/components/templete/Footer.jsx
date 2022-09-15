ecc0258b5dd31dfd1b2ae74d87c1401461bf420d
Andredossantos96 <86696902+Andredossantos96@users.noreply.github.com>
2022-09-14 23:05:22 -0300

    Rodapé

--- cadastro/frontend/src/components/templete/Footer.jsx
+++ cadastro/frontend/src/components/templete/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> André dos Santos </strong>
+        </span>
     </footer>
